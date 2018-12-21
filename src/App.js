d85ac86043b0e7a97e9193bae0fb0cd284722d5e
Adolfo Vela <vela@tekplanner.com>
2018-12-20 21:13:26 -0600

    Se coloca el theme de material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
-import SlideBar from './components/slidebar/Slidebar'
+//import SlideBar from './components/slidebar/Slidebar'
+import { Router, Route, Switch } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+
+import indexRoutes from "./routes/index";
+const hist = createBrowserHistory();
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SlideBar />
-      </div>
+      <Router history={hist}>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return <Route path={prop.path} component={prop.component} key={key} />;
+        })}
+      </Switch>
+    </Router>
     );
   }
 }
---
+++ src/routes/index.jsx
@@ -0,0 +1,11 @@
+import Pages from "../components/layouts/Pages.jsx";
+// import RTL from "../components/layouts/RTL.jsx";
+// import Dashboard from "layouts/Dashboard.jsx";
+
+var indexRoutes = [
+  // { path: "/rtl", name: "RTL", component: RTL },
+  { path: "/pages", name: "Pages", component: Pages },
+  // { path: "/", name: "Home", component: Dashboard }
+];
+
+export default indexRoutes;
